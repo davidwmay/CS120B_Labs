e7ea620f4319b89dc33de5d4877691d3cfb6dd95
davidwmay <40504054+davidwmay@users.noreply.github.com>
2019-04-08 10:52:40 -0700

    First commit of exercise 2

--- Lab2/dmay004_lab3_part2/dmay004_lab3_part2/main.c
+++ Lab2/dmay004_lab3_part2/dmay004_lab3_part2/main.c
@@ -8,11 +8,16 @@
 #include <avr/io.h>
 
 
-int main(void)
-{
-    /* Replace with your application code */
-    while (1) 
-    {
-    }
-}
 
+int main() {
+	DDRA = 0x00; PORTA = 0xFF; // output, set depending on PORTC values
+	DDRC = 0xFF; PORTC = 0x00; // input, check to find fuel level 
+	unsigned char tmpB = 
+	unsigned char tmpC = 0x00;
+	
+	while (1) {
+		
+	}
+	
+	return 0;
+}
