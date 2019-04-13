4e7d5ed71c7bb4ab5b635b34cf1f8efbcf02c0a1
David May <dmay004@ucr.edu>
2019-04-12 20:28:53 -0700

    Maybe completed part 1

---
+++ Lab4/dmay004_lab4_part1/dmay004_lab4_part1/main.c
@@ -0,0 +1,74 @@
+/*	Partner 1 Name & E-mail: David May; dmay004@ucr.edu
+ *	Partner 2 Name & E-mail: 
+ *	Lab Section: 026
+ *	Assignment: Lab 4  Exercise 1 
+ *	
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+
+#include <avr/io.h>
+
+enum States{start, ON1, ON2} state;
+
+unsigned char tmpA = 0x00;
+unsigned char tmpB = 0x00;
+
+void sequence(){
+	tmpA = PINA & 0x01;
+
+	switch (state){
+		case start:
+		state = ON1;
+		break;
+		case ON1:
+		if(tmpA){
+			state = ON2;
+		}
+		else if (!tmpA){
+			state = ON1;
+		}
+		break;
+		case ON2:
+		if(tmpA){
+			state = ON1;
+		}
+		else if(!tmpA){
+			state = ON2;
+		}
+		break;
+		default:
+		state = start;
+		break;
+	}
+	
+	switch (state){
+		case start:
+		break;
+		case ON1:
+		tmpB = 0x01;
+		break;
+		case ON2:
+		tmpB = 0x02;
+		break;
+		default:
+		break;
+	}
+	
+	PORTB = tmpB;
+}
+
+
+int main(void)
+{
+	/* Replace with your application code */
+	DDRA = 0x00; PORTA = 0xFF;
+	DDRB = 0xFF; PORTB = 0x00;
+	
+	tmpB = 0x00;
+	state = start;
+	while (1)
+	{
+		sequence();
+	}
+}
